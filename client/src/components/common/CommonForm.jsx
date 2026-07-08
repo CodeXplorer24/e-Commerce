37232420909758c2745d7796f3fe94b30b95aa45
Jyt-deva <anandjyotish4@gmial.com>
2026-07-08 22:08:33 +0530

    feat(common): Add CommonForm component with dynamic input rendering

---
+++ client/src/components/common/CommonForm.jsx
@@ -0,0 +1,118 @@
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { Button } from "../ui/button";
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem} from "../ui/select";
+import { Textarea } from "../ui/textarea";
+
+function CommonForm({
+    formControls,
+    onSubmit,
+    formData,
+    setFormData,
+    buttonText,
+    isBtnDisabled
+}) {
+
+    function renderInputsByComponentType(controlItem) {
+      let element;
+      const value = formData?.[controlItem.name] || "";
+      switch (controlItem.componentType) {
+        case "input":
+          element = (
+            <Input
+              id={controlItem.name}
+              name={controlItem.name}
+              placeholder={controlItem.placeholder}
+              type={controlItem.type}
+              value={value}
+              onChange={(event) => setFormData({
+                ...formData,
+                [controlItem.name]: event.target.value,
+              }
+              )}
+            />
+          );
+          break;
+        case "select":
+          element = (
+            <Select>
+                value={value}
+                onValueChange={(event) => setFormData({
+                    ...formData,
+                    [controlItem.name]: event.target.value
+                })
+                }
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder={controlItem.placeholder} />
+              </SelectTrigger>
+              <SelectContent>
+                {controlItem.options && controlItem.options.length > 0
+                  ? controlItem.options.map((optionItem) => (
+                      <SelectItem key={optionItem.id} value={optionItem.id}>
+                        {optionItem.label}
+                      </SelectItem>
+                    ))
+                  : null}
+              </SelectContent>
+            </Select>
+          );
+          break;
+        case "textarea":
+          element = (
+            <Textarea
+              value={value}
+              onChange={(event) => setFormData({
+                ...formData,
+                [controlItem.name]: event.target.value,
+              }
+              )}
+              id={controlItem.name}
+              name={controlItem.name}
+              placeholder={controlItem.placeholder}
+              type={controlItem.type}
+            />
+          );
+          break;
+        default:
+          element = (
+            <Input
+              value={value}
+              onChange={(event) => setFormData({
+                ...formData,
+                [controlItem.name]: event.target.value,
+              }
+              )}
+              id={controlItem.name}
+              name={controlItem.name}
+              placeholder={controlItem.placeholder}
+              type={controlItem.type}
+            />
+          );
+          break;
+      }
+      return element;
+    }
+    return ( 
+        <form onSubmit={onSubmit}>
+            <div className="flex flex-col gap-3">
+                {
+                    formControls.map((controlItem) => 
+                        <div className="grid w-full gap-2">
+                            <Label className="mb-1">{controlItem.label}</Label>
+                            {
+                                renderInputsByComponentType(controlItem)
+                            }
+                        </div>
+                    )
+                }
+            </div> 
+            <div className="flex justify-center">
+            <Button disabled={isBtnDisabled} className="mt-2 w-fit">
+                {buttonText || 'Submit'}
+            </Button>
+            </div>
+        </form>
+     );
+}
+
+export  {CommonForm};
