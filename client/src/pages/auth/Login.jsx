08b3490329e6e7037aa30f73d7bd79f7b299168b
Jyt-deva <anandjyotish4@gmial.com>
2026-07-31 01:14:13 +0530

    feat(auth): Refactor Login and Register components to use CommonForm and improve user experience

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,10 +1,62 @@
+import { loginFormControls } from "@/config/form_controller.js";
+import { Link } from "react-router-dom";
+import {CommonForm} from "@/components/common/CommonForm.jsx";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+// import  {useNavigate} from "react-router-dom"
+import { loginUser } from "@/features/auth/authThunk";
+import { toast } from "sonner";
+
+const loginInitialState = {
+    email: "",
+    password: ""
+}
 
 function Login() {
-    return ( 
-        <div>
-            <h1>Login</h1>
-        </div>
-    );
+  const [formData, setFormData] = useState(loginInitialState);
+  const dispatch = useDispatch();
+  // const navigate = useNavigate();
+
+  function onSubmit(event){
+    event.preventDefault();
+    dispatch(loginUser(formData))
+      .then((data) => {
+        // console.log("Full thunk action data",data);
+        // console.log("Backend response metada",data.payload);
+        // console.log("User object From response payload", data.payload.data);
+        
+        if (data?.payload?.success) {
+          toast.success(data?.payload?.message);
+          // navigate();
+        } 
+        else {
+          toast.error(data?.payload?.message);
+        }
+      })
+  }
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+        Log into Shopingram
+        </h1>
+        <p className="mt-2">
+          Don't have an account
+          <Link className="font-medium ml-2 text-primary hover:underline"
+          to="/auth/register">
+            Register
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={loginFormControls}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+        buttonText={"Sign In"}
+      />
+    </div>
+  );
 }
 
 export default Login
--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,11 +1,63 @@
+import { useState } from "react"
+import {CommonForm} from "@/components/common/CommonForm.jsx";
+import { registerFormControls } from "../../config/form_controller.js";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { toast } from "sonner";
+import { registerUser } from "@/features/auth/authThunk.js";
 
+const initialRegisterState = {
+  username: "",
+  email: "",
+  password: ""
+}
 
 function Register() {
+  const [formData, setFormData] = useState(initialRegisterState);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+ 
+  function handleRegSubmit(event){
+    event.preventDefault();
+    dispatch(registerUser(formData)).then((data) => {
+      console.log(data);
+      console.log(formData);
+      
+      if(data?.payload?.success){
+        toast.success(data?.payload?.message)
+        navigate("/auth/login");
+      }
+      else {
+       toast.error(data?.payload?.message)
+      }
+    })
+  }
+  // console.log(formData);
+  
   return (
-    <div>
-      Register
+    <div className="mx-auto w-max max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Sign up for shopping
+        </h1>
+        <p className="mt-2">
+          Already have an account
+          <Link className="font-medium ml-2 text-primary hover:underline"
+          to="/auth/login">
+            Login
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={registerFormControls}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={handleRegSubmit}
+        buttonText={"Sign Up"}
+      />
     </div>
-  )
+  );
 }
 
 export default Register
